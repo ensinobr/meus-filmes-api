974481e6be4c477d2904f70209975d2e92d96aae
Bruno <bruno.sargi@accurate.com.br>
2018-11-26 14:35:16 -0200

    Criação do projeto

---
+++ src/main/java/br/com/ensinobr/meusfilmes/MeusFilmesApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.ensinobr.meusfilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusFilmesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeusFilmesApiApplication.class, args);
+	}
+}
